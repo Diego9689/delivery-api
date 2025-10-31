f87d2aaceea9577d9aab5bc2119e874f331115e6
Diego Vinicius <coelhodiego059@gmail.com>
2025-10-30 23:21:38 -0300

    Primeiro commit

---
+++ src/main/java/com/delivery_api/Projeto/delivey/API/ProjetoDeliveyApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.delivey.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeliveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeliveyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery_api/Projeto/delivey/API/ProjetoDeliveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.delivey.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeliveyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
