7f55fe6250ba248e2fac527a11fb4691c379e9e1
Diego Vinicius <coelhodiego059@gmail.com>
2025-10-31 14:40:08 -0300

    Cliente completo e BD

--- src/main/java/com/delivery_api/Projeto/delivey/API/controller/ClienteController.java
+++
@@ -1,4 +0,0 @@
-package com.delivery_api.Projeto.delivey.API.controller;
-
-public class ClienteController {
-}
--- src/test/java/com/delivery_api/Projeto/Delivery/API/ProjetoDeliveyApiApplicationTests.java
+++ src/test/java/com/delivery_api/Projeto/Delivery/API/ProjetoDeliveyApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.delivery_api.Projeto.delivey.API;
+package com.delivery_api.Projeto.Delivery.API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
